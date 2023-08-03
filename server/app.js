3f29d0cb165df0bc7b2da877be10af2654289bce
void0707 <moazzan56@gmail.com>
2023-08-04 02:28:58 +0500

    Schemas Changed

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+const Parser = require("rss-parser");
+const cron = require("node-cron");
 require("dotenv").config();
-app.use(cors());
+const cheerio = require("cheerio");
 
-const corsOptions = { origin: "https://tesla-frontend.onrender.com" };
+const app = express();
 app.use(express.json());
-//connect MongoDB
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -20,6 +20,59 @@ mongoose
     console.log(err);
   });
 
+// Define the schema and model for a Blog
+const blogSchema = new mongoose.Schema({
+  title: String,
+  story: String,
+  description: String,
+  date: Date,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+// Create an instance of the RSS parser
+let parser = new Parser();
+
+// The URL of the RSS feed to parse
+const RSS_URL = "http://feeds.feedburner.com/DriveTeslaCanada";
+
+let task = cron.schedule("* * * * *", () => {
+  // Runs every hour
+  (async () => {
+    let feed = await parser.parseURL(RSS_URL);
+
+    for (let item of feed.items) {
+      // Check if this post has been extracted before
+      let blog = await Blog.findOne({ title: item.title });
+
+      if (!blog) {
+        // Store the title in
+        let $ = cheerio.load(item["content:encoded"]);
+
+        blog = new Blog({
+          title: item.title,
+          description: item.contentSnippet,
+          story: $.text(),
+          date: item.isoDate,
+        });
+        console.log(item.contentSnippet);
+
+        await blog.save();
+      }
+    }
+  })();
+});
+
+task.start();
+const corsOptions = { origin: "http://localhost:3000" };
+// Create an express application and use cors
+
+app.use(cors(corsOptions));
 app.get("/", (req, res) => {
-  res.status(201).json({ message: "Connected To Backend" });
+  res.status(201).json({ message: "Connected" });
+});
+// Define an endpoint to get all blogs
+app.get("/blogs", async (req, res) => {
+  let blogs = await Blog.find({});
+  res.json(blogs);
 });
